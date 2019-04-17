0ac7d8a1476d3a0bd933484449ebfc907ce5c59e
jayer1 <jayer1@my.wctc.edu>
2019-04-17 18:31:25 -0500

    Initial commit

---
+++ src/bowlinggame/MyTestSuite.java
@@ -0,0 +1,12 @@
+package bowlinggame;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses (  {
+    GameTest.class, PlayersScoresTest.class
+}  )
+public class MyTestSuite {
+}
